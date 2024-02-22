c660de3f7c77a32302ed4d390e852cc5fa526667
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-22 10:24:37 +0530

    feat: Drop Down of Profile click added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,4 +1,14 @@
-import { MessageSquareText, Rocket, Search, Twitter } from "lucide-react";
+import {
+  Handshake,
+  HelpCircle,
+  LogOut,
+  MessageSquareMore,
+  MessageSquareText,
+  Rocket,
+  Search,
+  Twitter,
+  User,
+} from "lucide-react";
 import profileImg from "./../assets/batman.jpg";
 import { Link } from "react-router-dom";
 import {
@@ -7,8 +17,11 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
   return (
     <div className="flex w-full justify-between px-4 sm:px-6 md:px-8 py-2 glassMorphisom fixed top-0 left-0 z-10">
       <Link to={"/"}>
@@ -65,13 +78,66 @@ const Navbar = () => {
         </TooltipProvider>
 
         {/* Profile */}
-        <div className="w-10 h-10 hover:shadow-lg rounded-full transition-all duration-300 cursor-pointer">
+        <div
+          className="w-10 h-10 hover:shadow-lg rounded-full transition-all duration-300 cursor-pointer"
+          onClick={() => {
+            setIsOpen((prev) => !prev);
+          }}
+        >
           <img
             src={profileImg}
             alt=""
             className="rounded-full hover:shadow-lg"
           />
         </div>
+
+        {/* DropDown */}
+        {isOpen && (
+          <div className="w-[16rem] rounded-md flex flex-col shadow-lg h-fit bg-white absolute top-[3.2rem] right-6 sm:right-8 md:right-10 border py-1 px-2">
+            <Link
+              to={"/profile/:username"}
+              className="w-full flex transition-colors duration-300 items-center text-[1rem] cursor-pointer gap-2 px-2 rounded-md hover:bg-gray-100 py-2"
+            >
+              <User className="size-[1.1rem]" /> Profile
+            </Link>
+            <Link
+              to={"/tweets"}
+              className="flex sm:hidden w-full transition-colors duration-300 items-center text-[1rem] cursor-pointer gap-2 px-2 rounded-md hover:bg-gray-100 py-2"
+            >
+              <Twitter className="size-[1.1rem]" /> Share Tweet
+            </Link>
+            <Link
+              to={"/feedback"}
+              className="sm:hidden flex w-full transition-colors duration-300 items-center text-[1rem] cursor-pointer gap-2 px-2 rounded-md hover:bg-gray-100 py-2"
+            >
+              <MessageSquareText className="size-[1.1rem]" /> Feedback
+            </Link>
+            <Link
+              to={"/faq"}
+              className="w-full flex transition-colors duration-300 items-center text-[1rem]  cursor-pointer gap-2 px-2 hover:bg-gray-100 py-2"
+            >
+              <HelpCircle className="size-[1.1rem]" /> FAQs
+            </Link>
+            <Link
+              to={"/bussiness"}
+              className="w-full flex transition-colors duration-300 items-center text-[1rem]  cursor-pointer gap-2 px-2 hover:bg-gray-100 py-2"
+            >
+              <Handshake className="size-[1.1rem]" /> Bussiness Enquiry
+            </Link>
+            <Link
+              to={"/discord"}
+              className="w-full flex transition-colors duration-300 items-center text-[1rem]  cursor-pointer gap-2 px-2 hover:bg-gray-100 py-2"
+            >
+              <MessageSquareMore className="size-[1.1rem] mt-1" /> Discord
+            </Link>
+            <Link
+              to={"/bussiness"}
+              className="w-full flex transition-colors duration-300 items-center text-[1rem]  cursor-pointer gap-2 px-2 hover:bg-gray-100 py-2"
+            >
+              <LogOut className="size-[1.1rem]" /> Logout
+            </Link>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/WebsiteOfTheDay.tsx
+++ frontend/src/components/WebsiteOfTheDay.tsx
@@ -4,11 +4,11 @@ import { ArrowUpRightFromCircle } from "lucide-react";
 
 const WebsiteOfTheDay = () => {
   return (
-    <div className="w-full mt-10 h-[100vh] flex flex-col justify-center items-center">
+    <div className="w-full mt-12 h-[100vh] gap-1 flex flex-col justify-center items-center">
       <div className="w-[95%] px-2 flex justify-between">
         <h1 className="font-semibold uppercase">Abhay Panchal</h1>
         <Link to={"/website/:id"}>
-          <ArrowUpRightFromCircle className="size-5" />
+          <ArrowUpRightFromCircle className="size-5" strokeWidth={2.2} />
         </Link>
       </div>
       <div className="w-[95%] h-[95%] rounded-2xl border relative overflow-hidden">
