10f654d1d6a0281f3cbaec20769e8006a94c43f4
Abhay Panchal <abpanchal951357@gmail.com>
2024-02-22 09:18:25 +0530

    chore: normal changes

--- frontend/src/components/WebsiteOfTheDay.tsx
+++ frontend/src/components/WebsiteOfTheDay.tsx
@@ -1,21 +1,23 @@
 import { Link } from "react-router-dom";
-import tempBannerImg from "./../assets/Screenshot (447).png";
+import tempBannerImg from "./../assets/pp.png";
 import { ArrowUpRightFromCircle } from "lucide-react";
 
 const WebsiteOfTheDay = () => {
   return (
-    <div className="w-full mt-20 h-[100vh] flex justify-center items-center">
-      <div className="group w-[95%] h-[95%] rounded-2xl border relative overflow-hidden">
-        <Link to={"/website/website-id"}>
-          <img
-            src={tempBannerImg}
-            alt=""
-            className="scale-100 w-full h-full rounded-2xl object-cover group-hover:scale-110 transition-all duration-300"
-          />
-          <div className="w-full h-full rounded-2xl hover:opacity-100 opacity-0 transition-opacity duration-300 absolute bg-[rgba(0,0,0,0.5)] top-0 left-0 flex justify-center items-center">
-            <ArrowUpRightFromCircle className="size-8 text-white" />
-          </div>
+    <div className="w-full mt-10 h-[100vh] flex flex-col justify-center items-center">
+      <div className="w-[95%] px-2 flex justify-between">
+        <h1 className="font-semibold uppercase">Abhay Panchal</h1>
+        <Link to={"/website/:id"}>
+          <ArrowUpRightFromCircle className="size-5" />
         </Link>
+      </div>
+      <div className="w-[95%] h-[95%] rounded-2xl border relative overflow-hidden">
+        <img
+          src={tempBannerImg}
+          alt=""
+          className="w-full h-full rounded-2xl object-cover transition-all duration-300"
+        />
+        <div className="w-full h-full rounded-2xl absolute bg-[rgba(0,0,0,0.3)] top-0 left-0 flex justify-center items-center"></div>
       </div>
     </div>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import WebsiteOfTheDay from "@/components/WebsiteOfTheDay";
 const Home = () => {
   window.document.title = "W | Sitezz | Home";
   return (
-    <div className="w-full h-fit">
+    <div className="w-full h-fit py-10">
       <WebsiteOfTheDay />
     </div>
   );
